54e4cb719f8a1f94a3109684aedb96f2ccad70e1
Ian Kendall <iankendall17@gmail.com>
2023-04-10 15:48:41 -0500

    nav logo

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,7 @@
 import "../styles/App.css";
 import { BrowserRouter as Router, Route, Link, useParams, Routes } from "react-router-dom";
 import { Carriers } from "./carriers/Carriers";
-import { Grommet, Header, Nav, Text, Image, Avatar } from "grommet";
+import { Box, Grommet, Header, Nav, Text, Image, Avatar } from "grommet";
 import { hpe } from "grommet-theme-hpe";
 import { Toaster } from "react-hot-toast";
 import { Ports } from "./ports/Ports";
@@ -19,6 +19,28 @@ const App = () => (
       <Grommet full theme={hpe}>
         <Header background='dark-1' pad='small'>
           <Nav direction='row' align='left' gap='medium'>
+            <Link to='/'>
+              <Box direction='row'>
+                <Box>
+                  <Avatar size='medium' src='/favicon.ico' round='large' align='right' />
+                </Box>
+                <Box>
+                  <Text
+                    style={{
+                      color: "white",
+                      paddingLeft: 20,
+                      paddingTop: 10,
+                      fontSize: 20,
+                      fontWeight: "bold",
+                      letterSpacing: 1,
+                    }}
+                  >
+                    Draylite
+                  </Text>
+                </Box>
+              </Box>
+            </Link>
+            {/*
             {navItems.map((item) => (
               <Link to={item.href} key={item.label}>
                 <Text style={{ paddingLeft: 37 }} color='white'>
@@ -26,16 +48,20 @@ const App = () => (
                 </Text>
               </Link>
             ))}
+            */}
           </Nav>
-          <Link to='/'>
-            <Avatar size='medium' src='/favicon.ico' round='large' align='right' />
-          </Link>
         </Header>
         <Routes>
           <Route path='/port/:portId/carriers' element={<Carriers />} />
           <Route path='/' element={<Ports />} />
         </Routes>
       </Grommet>
+      {/* 
+          GLOBAL TOASTER
+        ======================
+          UX notifs all pop up from here. 
+          Use toast.succes(message) OR toast.error(message) 
+      */}
       <Toaster position='bottom-center' reverseOrder={false} />
     </Router>
   </>
