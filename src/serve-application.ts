e156f934b2664e28c5bfb8e27edf99fdd19d1970
Ian Kendall <iankendall17@gmail.com>
2023-05-05 17:53:17 -0500

    serve spa quickfix

--- src/serve-application.ts
+++ src/serve-application.ts
@@ -1,3 +1,5 @@
+import { http } from "@serverless/cloud";
+
 /*===============================================
 ONLY FOR USE with current Serverless Cloud setup. 
 -------------------------------------------------------------------
